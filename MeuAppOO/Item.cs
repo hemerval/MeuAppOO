b7c3e31dbd13417cf8acf79c79e0ef17b40847bf
HEMERVAL DA ROCHA VIANA <hemerval@outlook.com>
2024-01-11 10:21:33 -0300

    ITEM

---
+++ MeuAppOO/Item.cs
@@ -0,0 +1,72 @@
+﻿namespace MeuAppOO
+{
+    public class Item
+    {
+        private string _Nome { get; set; }
+        private int _Quantidade { get; set; }
+        private decimal _Preco { get; set; }
+        private decimal _Total { get; set; }
+
+        public Item(string nome, int quantidade, decimal preco)
+        {
+            Nome = nome;
+            Quantidade = quantidade;
+            Preco = preco;
+        }
+
+        public string Nome
+        {
+            get { return _Nome; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("O nome do item é invalido!".ToUpper());
+                }
+                else
+                {
+                    _Nome = value.ToUpper();
+                }
+            }
+        }
+        public int Quantidade
+        {
+            get { return _Quantidade; }
+            set
+            {
+                if (value <= 0)
+                {
+                    _Quantidade = 1;
+                }
+                else
+                {
+                    _Quantidade = value;
+                }
+            }
+        }
+        public decimal Preco
+        {
+            get { return _Preco; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("O Preço do item é invalido!".ToUpper());
+                }
+                else
+                {
+                    _Preco = value;
+                }
+            }
+        }
+
+        public decimal Total
+        {
+            get
+            {
+                _Total = Quantidade * Preco;
+                return _Total;
+            }
+        }
+    }
+}
--- MeuAppOO/Program.cs
+++ MeuAppOO/Program.cs
@@ -1,5 +1,19 @@
 ﻿
+using MeuAppOO;
+
 Console.WriteLine("#################### - INICIO - ##########");
+
+#region Item
+Item lapiz = new Item("Lapiz", 0, 1.99M);
+Item caneta = new Item("Caneta", 2, 1.98M);
+Item borracha = new Item("Borracha", 3, 1.97M);
+Item caderno = new Item("Caderno", 4, 1.96M);
+
+Console.WriteLine($" Nome: {lapiz.Nome}, Quantidade: {lapiz.Quantidade}, Preço: {lapiz.Preco}, Total: {lapiz.Total}!");
+
+
+
+#endregion
 
 Console.ReadLine();
 
