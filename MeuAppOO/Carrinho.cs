f114ea1dbb6260ec5feb50058787c0a4acabe481
HEMERVAL DA ROCHA VIANA <hemerval@outlook.com>
2024-01-11 11:04:28 -0300

    CARRINHO

---
+++ MeuAppOO/Carrinho.cs
@@ -0,0 +1,38 @@
+﻿namespace MeuAppOO
+{
+    public class Carrinho
+    {
+        private List<Item> items;
+
+        public Carrinho()
+        {
+            items = new List<Item>();
+        }
+
+        public List<Item> Items
+        {
+            get { return items; }
+        }
+
+        public void AdicionarItem(Item item)
+        {
+            if (item != null)
+                items.Add(item);
+        }
+
+        public void RemoverItem(Item item)
+        {
+            if (item != null)
+                items.Remove(item);
+        }
+
+        public int Quantidade
+        {
+            get { return items.Sum(x => x.Quantidade); }
+        }
+        public decimal Total
+        {
+            get { return items.Sum(x => x.Quantidade * x.Total); }
+        }
+    }
+}
--- MeuAppOO/Program.cs
+++ MeuAppOO/Program.cs
@@ -8,13 +8,41 @@ Item lapiz = new Item("Lapiz", 0, 1.99M);
 Item caneta = new Item("Caneta", 2, 1.98M);
 Item borracha = new Item("Borracha", 3, 1.97M);
 Item caderno = new Item("Caderno", 4, 1.96M);
+/*
+List<Item> listItem = new List<Item>();
+listItem.Add(lapiz);
+listItem.Add(caneta);
+listItem.Add(borracha);
+listItem.Add(caderno);
 
-Console.WriteLine($" Nome: {lapiz.Nome}, Quantidade: {lapiz.Quantidade}, Preço: {lapiz.Preco}, Total: {lapiz.Total}!");
-
-
+foreach (Item item in listItem)
+{
+    Console.WriteLine($"Nome: {item.Nome}, Quantidade: {item.Quantidade}, Preço: {item.Preco}, Total: {item.Total}!");
+}
+*/
 
 #endregion
 
-Console.ReadLine();
+#region Carrinho
+Carrinho carrinho = new Carrinho();
+carrinho.AdicionarItem(lapiz);
+carrinho.AdicionarItem(caneta);
+carrinho.AdicionarItem(borracha);
+carrinho.AdicionarItem(caderno);
+
+foreach (Item item in carrinho.Items)
+{
+    while (item.Nome.Length <= 10) item.Nome += " ";
+
+    Console.WriteLine($"Nome: {item.Nome}, Quantidade: {item.Quantidade}, Preço: {item.Preco.ToString("C")}, Total: {item.Total}!".ToUpper());
+}
+
+Console.WriteLine();
+Console.WriteLine($"Quantidade total de itens do carrinho = {carrinho.Quantidade}!".ToUpper());
+Console.WriteLine($"Valor total de itens do carrinho = {carrinho.Total.ToString("C")}!".ToUpper());
+
+#endregion
 
 Console.WriteLine("#################### - FINAL - ##########");
+
+Console.ReadLine();
